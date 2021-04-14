be30928a4ca875b924a1bf39f03cfde3f59d3ab5
w0rt0x <t.filzer@gmx.de>
2021-04-14 17:52:07 +0200

    .

--- Skripte/Plotter.py
+++ Skripte/Plotter.py
@@ -1,22 +1,48 @@
-import seaborn as sns 
+import seaborn as sns
 import matplotlib as plt
 import numpy as np
-import pandas as pd 
-import csv 
-from scipy.io import loadmat 
+import pandas as pd
+import csv
+from scipy.io import loadmat
+import math
 
-def get_data(path):
-    """
-    Takes Path to csv-file, returns dictionary (day,trail) as key
-    """
-    
 
-def convert_data(path):
+def get_data(path_class, path_lact):
     """
-    converts data to csv-file with header-line
+    Reads in matlab files,
+    returns header list and neuron data
     """
-    return 0
+    # Getting labels
+    mat = loadmat(path_class)
+    days = mat['class_all_days'][0]
+    trials = mat['class_stim_evo'][0]
+    # Creating Header-List with (Day, Trial) Format
+    header = list(map(lambda d, t: (d, t), days, trials))
+
+    # Getting Neuron activity
+    mat = loadmat(path_lact)
+    data = []
+    for i in range(len(mat['vecs_all_days'])):
+        data.append(mat['vecs_all_days'][i])
+
+    # Swapping x and y coordinats in data so that one trial is in one list (x-axis)
+    # Swapping is done with .T from numpy: https://note.nkmk.me/en/python-list-transpose/
+    return header, np.array(data).T.tolist()
+
+
+def replace_nan(data, replacement):
+    """ 
+    replaces nan with replacement in data-matrix,
+    does not return list because lists are matuable
+    """
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if math.isnan(data[i][j]):
+                data[i][j] = replacement
 
 
 if __name__ == "__main__":
-    print("test")
+    header, data = get_data(r"C:\Users\Sam\Desktop\BachelorInfo\Bachelor-Info\Daten\bl660-1_two_white_Pop01_class.mat",
+                            r"C:\Users\Sam\Desktop\BachelorInfo\Bachelor-Info\Daten\bl660-1_two_white_Pop01_lact.mat")
+    replace_nan(data, 0)
+    
